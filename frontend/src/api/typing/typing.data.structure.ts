6f44666ee36f942ffb3d271d52a20c455fe33acc
tuntinotuan <tuntino666@gmail.com>
2025-04-14 21:09:49 +0700

    add typing structure datas

---
+++ frontend/src/api/typing/typing.data.structure.ts
@@ -0,0 +1,72 @@
+import { typingWordsTypes } from "./typing.type";
+
+export const typingwords: typingWordsTypes[] = [
+  {
+    word: "background",
+    meaning: "lý lịch, phần sau",
+  },
+  {
+    word: "chimney",
+    meaning: "ống khói",
+  },
+  {
+    word: "fireplace",
+    meaning: "lò sưởi",
+  },
+  {
+    word: "narrow",
+    meaning: "chật hẹp",
+  },
+  {
+    word: "circular",
+    meaning: "hình tròn, đi vòng quanh",
+  },
+  {
+    word: "railings",
+    meaning: "lan can, hàng rào song",
+  },
+  {
+    word: "stairway",
+    meaning: "cầu thang",
+  },
+  {
+    word: "corridor",
+    meaning: "hành lang",
+  },
+  {
+    word: "scattered",
+    meaning: "rải rác, phân tán",
+  },
+  {
+    word: "goods",
+    meaning: "hàng hoá",
+  },
+  {
+    word: "baskets",
+    meaning: "giỏ, rổ",
+  },
+  {
+    word: "fuel truck",
+    meaning: "xe tải, nhiên liệu",
+  },
+  {
+    word: "jet",
+    meaning: "máy bay phản lực",
+  },
+  {
+    word: "crane",
+    meaning: "cần cẩu",
+  },
+  {
+    word: "wet",
+    meaning: "ướt",
+  },
+  {
+    word: "conductor",
+    meaning: "nhạc trưởng, chỉ huy",
+  },
+  {
+    word: "conductor",
+    meaning: "nhạc trưởng, chỉ huy",
+  },
+];
