b1fb96502737c186f82475f712cc56a79bddcfaa
tuntinotuan <tuntino666@gmail.com>
2025-04-14 19:39:09 +0700

    update ui typing header menu

--- frontend/src/app/(home)/typing/page.tsx
+++ frontend/src/app/(home)/typing/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import SettingIcon from "@/components/icons/SettingIcon";
+import { Tooltip } from "@nextui-org/tooltip";
 import { useState } from "react";
-
+import AccessTimeFilledRoundedIcon from "@mui/icons-material/AccessTimeFilledRounded";
+import TextFormatIcon from "@mui/icons-material/TextFormat";
 export default function TypingPage() {
   const [text, setText] = useState<string>("");
   const [cursorPosition, setCursorPosition] = useState<number>(0);
@@ -18,10 +20,7 @@ export default function TypingPage() {
   };
   return (
     <div className="flex flex-col w-full gap-2 px-4 pt-2 bg-[#262A33] text-white">
-      <div className="flex items-center justify-between bg-gray-500 rounded-lg px-5 py-3">
-        <h1>Practice with your typing skill and remember your keyword</h1>
-        <SettingIcon></SettingIcon>
-      </div>
+      <TypingHeaderMenu></TypingHeaderMenu>
       <div className="w-full h-full flex flex-col items-center justify-center gap-4 ">
         <label
           htmlFor="typingCursor"
@@ -53,3 +52,39 @@ export default function TypingPage() {
     </div>
   );
 }
+
+const TypingHeaderMenu = () => {
+  return (
+    <div className="flex items-center gap-3 !w-auto mx-auto bg-[#1F232C] text-[#526777] rounded-lg px-5 py-3">
+      <Tooltip
+        showArrow
+        content="Practice with your typing skill and remember your keyword"
+        placement="bottom"
+        radius="sm"
+        delay={200}
+        closeDelay={200}
+        className="!px-2 !py-[2px]"
+        shadow="sm"
+      >
+        <h1 className="cursor-default">Typing</h1>
+      </Tooltip>
+
+      <div className="w-[6px] h-full bg-[#262A33] rounded-full"></div>
+      <div className="flex items-center gap-1 hover:text-white transition-all cursor-pointer">
+        <AccessTimeFilledRoundedIcon fontSize="inherit" />
+        time
+      </div>
+      <div className="flex items-center gap-1 hover:text-white transition-all cursor-pointer">
+        <TextFormatIcon fontSize="inherit" />
+        words
+      </div>
+      <div className="w-[6px] h-full bg-[#262A33] rounded-full"></div>
+      <p className="hover:text-white transition-all cursor-pointer">10</p>
+      <p className="hover:text-white transition-all cursor-pointer">25</p>
+      <p className="hover:text-white transition-all cursor-pointer">50</p>
+      <p className="hover:text-white transition-all cursor-pointer">100</p>
+      <div className="w-[6px] h-full bg-[#262A33] rounded-full"></div>
+      <SettingIcon></SettingIcon>
+    </div>
+  );
+};
