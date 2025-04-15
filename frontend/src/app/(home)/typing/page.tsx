47355f65498a1ba3cd0e8ad81e06672c6015ab0b
tuntinotuan <tuntino666@gmail.com>
2025-04-15 21:59:18 +0700

    update cursorPosition with text width

--- frontend/src/app/(home)/typing/page.tsx
+++ frontend/src/app/(home)/typing/page.tsx
@@ -5,69 +5,12 @@ import { useRef, useState } from "react";
 import AccessTimeFilledRoundedIcon from "@mui/icons-material/AccessTimeFilledRounded";
 import TextFormatIcon from "@mui/icons-material/TextFormat";
 import { typingwords } from "@/api/typing/typing.data.structure";
+
 export default function TypingPage() {
-  const [text, setText] = useState<string>("");
-  const [cursorPosition, setCursorPosition] = useState<number>(0);
-  const [currentTyping, setCurrentTyping] = useState(typingwords[0]);
-  const ref = useRef(1);
-
-  console.log("currentTyping", currentTyping);
-
-  const handleChangeInput = (e: any) => {
-    if (e.target.value === " ") return;
-    setText(e.target.value);
-    if (text.length < currentTyping.word.length) {
-      setCursorPosition(20 * e.target.value.length + 1);
-    } else {
-      setCursorPosition(cursorPosition);
-    }
-  };
   return (
     <div className="flex flex-col w-full gap-2 px-4 pt-2 bg-[#262A33] text-white">
       <TypingHeaderMenu></TypingHeaderMenu>
-      <div className="w-full h-full flex flex-col items-center justify-center gap-4 ">
-        <label
-          htmlFor="typingCursor"
-          className="relative flex items-center text-4xl text-[#526777] cursor-pointer"
-        >
-          {currentTyping.word.split("").map((item, index) => (
-            <div
-              key={index}
-              className={`${
-                currentTyping.word[index] === text.split("")[index]
-                  ? "text-white"
-                  : text.split("")[index] !== undefined
-                  ? "text-[#E9595A]"
-                  : ""
-              }`}
-            >
-              {item}
-            </div>
-          ))}
-          <input
-            value={text}
-            className="absolute top-0 bottom-0 w-[2px] rounded h-full bg-[#43FFAF] text-transparent opacity-0 focus:opacity-100 focus:animate-hideShow transition-all"
-            id="typingCursor"
-            onChange={handleChangeInput}
-            style={{ left: cursorPosition }}
-            onKeyDown={(e) => {
-              console.log("e.key", e.key);
-              if (e.key === " ") {
-                setCurrentTyping(typingwords[ref.current]);
-                setCursorPosition(0);
-                setText("");
-                if (typingwords[ref.current + 1] !== undefined) {
-                  ref.current = ref.current + 1;
-                } else {
-                  ref.current = 1;
-                  setCurrentTyping(typingwords[0]);
-                }
-              }
-            }}
-          />
-        </label>
-        <span className="text-2xl">{currentTyping.meaning}</span>
-      </div>
+      <TypingContent></TypingContent>
     </div>
   );
 }
@@ -104,6 +47,90 @@ const TypingHeaderMenu = () => {
       <p className="hover:text-white transition-all cursor-pointer">100</p>
       <div className="w-[6px] h-full bg-[#262A33] rounded-full"></div>
       <SettingIcon></SettingIcon>
+    </div>
+  );
+};
+
+const TypingContent = () => {
+  const [text, setText] = useState<string>("");
+  const [cursorPosition, setCursorPosition] = useState<number>(0);
+  const [currentTyping, setCurrentTyping] = useState(typingwords[0]);
+  const ref = useRef(1);
+
+  console.log("currentTyping", currentTyping);
+
+  function getTextWidth(text: string, font: string = "16px Arial"): number {
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+
+    if (!context) {
+      throw new Error("Failed to get canvas context");
+    }
+
+    context.font = font;
+    return context.measureText(text).width;
+  }
+
+  const handleChangeInput = (e: any) => {
+    if (e.target.value === " ") return;
+    setText(e.target.value);
+  };
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center gap-4 ">
+      <label
+        htmlFor="typingCursor"
+        className="relative flex items-center text-4xl text-[#526777] cursor-pointer"
+      >
+        {currentTyping.word.split("").map((item, index) => (
+          <div
+            key={index}
+            className={`${
+              currentTyping.word[index] === text.split("")[index]
+                ? "text-white"
+                : text.split("")[index] !== undefined
+                ? "text-[#E9595A]"
+                : ""
+            }`}
+          >
+            {item}
+          </div>
+        ))}
+        <input
+          value={text}
+          className="absolute top-0 bottom-0 w-[2px] rounded h-full bg-[#43FFAF] text-transparent opacity-0 focus:opacity-100 focus:animate-hideShow transition-all"
+          id="typingCursor"
+          onChange={handleChangeInput}
+          style={{ left: cursorPosition }}
+          onKeyDown={(e) => {
+            if (e.key === " ") {
+              setCurrentTyping(typingwords[ref.current]);
+              setCursorPosition(0);
+              setText("");
+              if (typingwords[ref.current + 1] !== undefined) {
+                ref.current = ref.current + 1;
+              } else {
+                ref.current = 1;
+                setCurrentTyping(typingwords[0]);
+              }
+            }
+            const textWidthIncrease = getTextWidth(
+              currentTyping.word[text ? text.length : 0],
+              "36px Arial"
+            );
+            const textWidthDecrease = getTextWidth(
+              currentTyping.word[text ? text.length - 1 : 0],
+              "36px Arial"
+            );
+            if (text.length < currentTyping.word.length && e.key !== " ") {
+              e.key !== "Backspace" &&
+                setCursorPosition(cursorPosition + textWidthIncrease + 1.5);
+            }
+            if (e.key === "Backspace" && text.length > 0)
+              setCursorPosition(cursorPosition - textWidthDecrease - 1.5);
+          }}
+        />
+      </label>
+      <span className="text-2xl">{currentTyping.meaning}</span>
     </div>
   );
 };
