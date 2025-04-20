b24c06f42f921130110b3da57829953817fe27bf
tuntinotuan <tuntino666@gmail.com>
2025-04-21 01:29:00 +0700

    update press any key and focus cursor

--- frontend/src/app/(home)/typing/modules/TypingOverlayBlur.tsx
+++ frontend/src/app/(home)/typing/modules/TypingOverlayBlur.tsx
@@ -1,15 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import AdsClickRoundedIcon from "@mui/icons-material/AdsClickRounded";
 
 const TypingOverlayBlur = ({ htmlFor }: { htmlFor: string }) => {
   const [hide, setHide] = useState(false);
+  const labelRef = useRef<HTMLLabelElement>(null);
+
+  useEffect(() => {
+    labelRef.current?.focus();
+  }, []);
   return (
     <label
+      ref={labelRef}
+      tabIndex={0}
       htmlFor={htmlFor}
       className={`absolute left-0 right-0 h-96 flex items-center justify-center gap-3 bg-[#262A33] bg-opacity-20 backdrop-blur-[9px] cursor-pointer transition-all ${
         hide ? "opacity-0 invisible" : "opacity-100 visible"
       }`}
       onClick={() => setHide(true)}
+      onKeyDown={(e) => {
+        console.log("overlay", e.key);
+        document.getElementById(htmlFor)?.focus();
+        setHide(true);
+      }}
     >
       <AdsClickRoundedIcon />
       <p>Click here or press any key to focus</p>
