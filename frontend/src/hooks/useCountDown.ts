c90413d68457387d3a95e0a26294583791ff3dd2
tuntinotuan <tuntino666@gmail.com>
2025-04-27 18:42:39 +0700

    update useCountDown hook

---
+++ frontend/src/hooks/useCountDown.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+export function useCountDown() {
+  const [seconds, setSeconds] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout | null = null;
+
+    if (isRunning) {
+      interval = setInterval(() => {
+        setSeconds((prev) => prev + 1);
+      }, 1000);
+    }
+
+    return () => {
+      if (interval) clearInterval(interval);
+    };
+  }, [isRunning]);
+
+  return { seconds, setIsRunning };
+}
