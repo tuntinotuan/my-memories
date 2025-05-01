86738de9ba80bd4438900429ea66930d73a1459f
tuntinotuan <tuntino666@gmail.com>
2025-05-01 22:01:41 +0700

    update add a pair of word functions

--- frontend/src/components/popup/PopupCreateTypingList.tsx
+++ frontend/src/components/popup/PopupCreateTypingList.tsx
@@ -29,12 +29,16 @@ const PopupCreateTypingList = ({
 
 const Body = ({ onClose }: any) => {
   const [typingList, setTypingList] = useState<typingWordsTypes[]>([]);
-  const handleAdd = () => {
+  const [word, setWord] = useState("");
+  const [meaning, setMeaning] = useState("");
+  const handleAddAPairOfWord = () => {
     const newWord = {
-      word: "",
-      meaning: "",
+      word,
+      meaning,
     };
     setTypingList([...typingList, newWord]);
+    setWord("");
+    setMeaning("");
   };
   return (
     <div className="flex flex-col gap-2 h-full w-full overflow-auto px-4 pb-4">
@@ -49,49 +53,60 @@ const Body = ({ onClose }: any) => {
       <div className="flex items-start gap-2 w-full">
         <div className="flex-1 flex flex-col gap-1">
           <input
+            value={word}
             type="text"
             className="border border-gray-300 rounded w-full px-3 py-2 focus:border-primaryColor transition-all"
             placeholder="word name..."
+            onChange={(e) => setWord(e.target.value)}
             required
           />
           <input
+            value={meaning}
             type="text"
             className="border border-gray-300 rounded w-full px-3 py-2 focus:border-primaryColor transition-all"
             placeholder="meaning of word..."
+            onChange={(e) => setMeaning(e.target.value)}
             required
           />
           <Button
             className="bg-typingBgControlMenu"
             hover=" hover:bg-typingColorActive"
-            onClick={handleAdd}
+            onClick={handleAddAPairOfWord}
           >
             <PlusIcon></PlusIcon>Add a pair of word
           </Button>
+          {typingList.length > 0 && (
+            <div className="ml-auto px-2 border border-gray-200 border-dotted rounded">
+              {typingList.length}
+            </div>
+          )}
         </div>
-        <div className="flex flex-col items-center justify-center gap-1 w-1/2 max-h-[150px] border border-gray-200 border-dotted rounded p-2 overflow-y-auto">
-          {/* <p className="text-[10px] text-typingTextWrong">
-            Nothing is imported
-          </p> */}
-          <div className="flex flex-col items-center border border-gray-200 border-dotted p-1 rounded">
-            <p className="text-typingTextNormal">Background</p>
-            <p className="text-[9px] text-typingTextHover">lý lịch, phần sau</p>
-          </div>
-          <div className="flex flex-col items-center border border-gray-200 border-dotted p-1 rounded">
-            <p className="text-typingTextNormal">Background</p>
-            <p className="text-[9px] text-typingTextHover">lý lịch, phần sau</p>
-          </div>
-          <div className="flex flex-col items-center border border-gray-200 border-dotted p-1 rounded">
-            <p className="text-typingTextNormal">Background</p>
-            <p className="text-[9px] text-typingTextHover">lý lịch, phần sau</p>
-          </div>
+        <div className="flex flex-col items-center justify-start gap-1 w-1/2 max-h-[150px] border border-gray-200 border-dotted rounded p-2 overflow-y-scroll">
+          {typingList.length === 0 && (
+            <p className="text-[10px] text-typingTextWrong">
+              Nothing is imported
+            </p>
+          )}
+          {typingList.length > 0 &&
+            typingList.map((item, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center border border-gray-200 border-dotted py-1 px-2 rounded"
+              >
+                <p className="text-typingTextNormal">{item.word}</p>
+                <p className="text-[9px] text-typingTextHover">
+                  {item.meaning}
+                </p>
+              </div>
+            ))}
         </div>
       </div>
       <label htmlFor="">Other options</label>
       <div className="flex gap-2">
-        <div className="cursor-pointer hover:bg-typingBgControlMenu trnasition-all p-2 rounded">
+        <div className="cursor-pointer hover:bg-typingBgControlMenu transition-all p-2 rounded">
           .txt files
         </div>
-        <div className="cursor-pointer hover:bg-typingBgControlMenu trnasition-all p-2 rounded">
+        <div className="cursor-pointer hover:bg-typingBgControlMenu transition-all p-2 rounded">
           .xlsx files
         </div>
       </div>
