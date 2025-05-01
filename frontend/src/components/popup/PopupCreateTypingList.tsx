a0c477cb8a31651d583bc76bf1410b6f83ad5bdc
tuntinotuan <tuntino666@gmail.com>
2025-05-01 22:22:35 +0700

    update create list functions

--- frontend/src/components/popup/PopupCreateTypingList.tsx
+++ frontend/src/components/popup/PopupCreateTypingList.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PopupOverlay from "./popup.overlay";
 import { TopControl } from "./PopupCreateboard";
 import Button from "../button/Button";
 import PlusIcon from "../icons/PlusIcon";
 import { typingWordsTypes } from "@/api/typing/typing.type";
+import { generateId } from "@/utils/otherFs";
 
 type PopupCreateTypingListProps = {
   show: boolean;
@@ -28,10 +29,34 @@ const PopupCreateTypingList = ({
 };
 
 const Body = ({ onClose }: any) => {
+  const [wordList, setWordList] = useState<any>([]);
   const [typingList, setTypingList] = useState<typingWordsTypes[]>([]);
   const [word, setWord] = useState("");
   const [meaning, setMeaning] = useState("");
+
+  useEffect(() => {
+    async function fetchWordListFromLocalStorage() {
+      let lists = null;
+      try {
+        const stored = localStorage.getItem("wordList");
+        if (stored) lists = await JSON.parse(stored);
+      } catch (error) {
+        console.error("Invalid JSON:", error);
+      }
+      if (lists !== null && lists.length > 0) {
+        setWordList(lists);
+      }
+    }
+    fetchWordListFromLocalStorage();
+  }, []);
+
+  useEffect(() => {
+    if (wordList.length <= 0) return;
+    localStorage.setItem("wordList", JSON.stringify(wordList));
+  }, [wordList]);
+
   const handleAddAPairOfWord = () => {
+    if (!word || !meaning) return;
     const newWord = {
       word,
       meaning,
@@ -39,6 +64,15 @@ const Body = ({ onClose }: any) => {
     setTypingList([...typingList, newWord]);
     setWord("");
     setMeaning("");
+  };
+  const handleCreateTypingList = () => {
+    if (typingList.length <= 0) return;
+    const newList = {
+      id: generateId(),
+      typingList,
+    };
+    setWordList([...wordList, newList]);
+    setTypingList([]);
   };
   return (
     <div className="flex flex-col gap-2 h-full w-full overflow-auto px-4 pb-4">
@@ -55,7 +89,7 @@ const Body = ({ onClose }: any) => {
           <input
             value={word}
             type="text"
-            className="border border-gray-300 rounded w-full px-3 py-2 focus:border-primaryColor transition-all"
+            className="border border-gray-300 rounded w-full px-3 py-2 focus:border-typingColorActive transition-all"
             placeholder="word name..."
             onChange={(e) => setWord(e.target.value)}
             required
@@ -63,7 +97,7 @@ const Body = ({ onClose }: any) => {
           <input
             value={meaning}
             type="text"
-            className="border border-gray-300 rounded w-full px-3 py-2 focus:border-primaryColor transition-all"
+            className="border border-gray-300 rounded w-full px-3 py-2 focus:border-typingColorActive transition-all"
             placeholder="meaning of word..."
             onChange={(e) => setMeaning(e.target.value)}
             required
@@ -81,7 +115,7 @@ const Body = ({ onClose }: any) => {
             </div>
           )}
         </div>
-        <div className="flex flex-col items-center justify-start gap-1 w-1/2 max-h-[150px] border border-gray-200 border-dotted rounded p-2 overflow-y-scroll">
+        <div className="flex flex-col items-center justify-start gap-1 w-1/2 max-h-[150px] border border-gray-200 border-dotted rounded p-2 overflow-y-auto">
           {typingList.length === 0 && (
             <p className="text-[10px] text-typingTextWrong">
               Nothing is imported
@@ -110,7 +144,12 @@ const Body = ({ onClose }: any) => {
           .xlsx files
         </div>
       </div>
-      <Button hover="hover:bg-typingBgControlMenu">Create list</Button>
+      <Button
+        hover="hover:bg-typingBgControlMenu"
+        onClick={handleCreateTypingList}
+      >
+        Create list
+      </Button>
     </div>
   );
 };
