12b404e4968f9091c45f1ee0e3a51c1884d3f406
tuntinotuan <tuntino666@gmail.com>
2025-05-31 22:45:00 +0700

    optimazation - Button show and hidden data component

---
+++ frontend/src/components/button/ButtonShowAndHiddenData.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Button from "./Button";
+import ArrowDownIcon from "../icons/ArrowDownIcon";
+import ArrowRightIcon from "../icons/ArrowRightIcon";
+
+type ButtonShowAndHiddenDataProps = {
+  list: any[];
+  title: string;
+  children: React.ReactNode;
+};
+
+const ButtonShowAndHiddenData = ({
+  list,
+  title,
+  children,
+}: ButtonShowAndHiddenDataProps) => {
+  const [show, setShow] = useState(true);
+  return (
+    <>
+      {list.length > 0 && (
+        <Button
+          className="group text-xs !gap-1 !py-[6px] !px-2 !rounded-[4px] mb-[14px] text-primaryText"
+          hover="hover:bg-primaryHover"
+          onClick={() => setShow((pre) => !pre)}
+        >
+          <p>{title}</p>
+          {show ? <ArrowDownIcon /> : <ArrowRightIcon />}
+        </Button>
+      )}
+      {show && list.length > 0 && (
+        <>
+          {children}
+          {list.length > 4 && (
+            <Button
+              className="w-full hover:bg-primaryHover text-primaryColor"
+              disable
+            >
+              See all
+            </Button>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default ButtonShowAndHiddenData;
