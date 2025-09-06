cee56cad07ca78c90ee2a656015e0d111599a4fe
tuntinotuan <tuntino666@gmail.com>
2025-09-06 17:42:24 +0700

    update - optimization components of TypingPageSkleton

--- frontend/src/components/skeleton/TypingPageSkeleton.tsx
+++ frontend/src/components/skeleton/TypingPageSkeleton.tsx
@@ -4,32 +4,45 @@ import React from "react";
 const TypingPageSkeleton = () => {
   return (
     <div className="flex flex-col items-start w-full gap-2 px-4 pt-2 bg-typingBg text-black">
-      <div className="flex items-center flex-wrap gap-3 !w-auto mx-auto bg-typingBgControlMenu text-typingTextNormal rounded-lg px-5 py-2 z-20 shadow-lg">
-        <div className="bg-gray-200 rounded w-[70px] h-6 animate-pulse"></div>
-        <SplitElement />
-        {Array(3)
-          .fill(null)
-          .map((item, index) => (
-            <div className="bg-gray-200 rounded w-[50px] h-6 animate-pulse"></div>
-          ))}
-        <SplitElement />
-        {Array(4)
-          .fill(null)
-          .map((item, index) => (
-            <div className="bg-gray-200 rounded w-[24px] h-6 animate-pulse"></div>
-          ))}
-        <SplitElement />
-        <div className="bg-gray-200 rounded w-[50px] h-6 animate-pulse"></div>
-        <SplitElement />
-        <div className="bg-gray-200 rounded w-[50px] h-6 animate-pulse"></div>
-      </div>
-      <div className="w-full h-full flex flex-col items-center justify-center gap-4 overflow-hidden">
-        <div className="flex items-end justify-between h-[20vh] bg-opacity-5 backdrop-blur-sm w-full z-10 p-2 rounded">
-          <div className="flex items-center gap-1 text-xl text-typingColorActive bg-typingBgControlMenu transition-all rounded py-1 px-2 shadow-lg">
-            <div className="bg-gray-200 rounded w-[24px] h-6 animate-pulse"></div>
-            /
-            <div className="bg-gray-200 rounded w-[24px] h-6 animate-pulse"></div>
-          </div>
+      <HeaderItems />
+      <AmountOrTimeItems />
+      <WordItems />=
+    </div>
+  );
+};
+
+const HeaderItems = () => {
+  return (
+    <div className="flex items-center flex-wrap gap-3 !w-auto mx-auto bg-typingBgControlMenu text-typingTextNormal rounded-lg px-5 py-2 z-20 shadow-lg">
+      <div className="bg-gray-200 rounded w-[70px] h-6 animate-pulse"></div>
+      <SplitElement />
+      {Array(3)
+        .fill(null)
+        .map((item, index) => (
+          <div className="bg-gray-200 rounded w-[50px] h-6 animate-pulse"></div>
+        ))}
+      <SplitElement />
+      {Array(4)
+        .fill(null)
+        .map((item, index) => (
+          <div className="bg-gray-200 rounded w-[24px] h-6 animate-pulse"></div>
+        ))}
+      <SplitElement />
+      <div className="bg-gray-200 rounded w-[50px] h-6 animate-pulse"></div>
+      <SplitElement />
+      <div className="bg-gray-200 rounded w-[50px] h-6 animate-pulse"></div>
+    </div>
+  );
+};
+
+const AmountOrTimeItems = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center gap-4 overflow-hidden">
+      <div className="flex items-end justify-between h-[20vh] bg-opacity-5 backdrop-blur-sm w-full z-10 p-2 rounded">
+        <div className="flex items-center gap-1 text-xl text-typingColorActive bg-typingBgControlMenu transition-all rounded py-1 px-2 shadow-lg">
+          <div className="bg-gray-200 rounded w-[24px] h-6 animate-pulse"></div>
+          /
+          <div className="bg-gray-200 rounded w-[24px] h-6 animate-pulse"></div>
         </div>
       </div>
     </div>
