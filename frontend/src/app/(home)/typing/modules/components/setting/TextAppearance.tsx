32339fb620c1c3ea44c1a2367f6512a3e6f1ade9
tuntinotuan <tuntino666@gmail.com>
2025-10-02 20:48:33 +0700

    optimization - TextAppearance component

--- frontend/src/app/(home)/typing/modules/components/setting/TextAppearance.tsx
+++ frontend/src/app/(home)/typing/modules/components/setting/TextAppearance.tsx
@@ -8,7 +8,6 @@ import { useTyping } from "@/contexts/TypingStates";
 import { CursorStyles } from "../../types";
 import { useAutoText } from "../../../func/setting/useAutoText";
 import { useLayoutStates } from "@/contexts/layoutStates";
-import { calculatePositionForCursor } from "../../../func/wordOlderV1/calculatePositionForCursor";
 import TextBoxBorderOverlay from "@/components/overlay/text.box.border.overlay";
 import TypingWordNew from "../TypingWordNew";
 import TypingCursorNew from "../TypingCursorNew";
@@ -23,6 +22,18 @@ import { useUpdateFirstTime } from "../../../func/setting/useUpdateFirstTime";
 import InputChangeFontsize from "./InputChangeFontsize";
 
 const TextAppearance = ({ show }: any) => {
+  const {
+    rect,
+    setRect,
+    setCursorShape,
+    typingSettingLocal,
+    textIsLowercase,
+    setTextIsLowercase,
+    typingFontsize,
+    wordGap,
+    typingFontsizeX,
+    setTypingFontsizeX,
+  } = useTyping();
   const wordList: typingWordsTypes[] = [
     {
       word: "I",
@@ -45,29 +56,7 @@ const TextAppearance = ({ show }: any) => {
       meaning: "",
     },
   ];
-  const {
-    rect,
-    setRect,
-    setCursorShape,
-    typingSettingLocal,
-    textIsLowercase,
-    setTextIsLowercase,
-    typingFontsize,
-    wordGap,
-    typingFontsizeX,
-    setTypingFontsizeX,
-  } = useTyping();
-  const [value, setValue] = useState("");
-  const [typingWordIndex, setTypingWordIndex] = useState(0);
-  const [resetComponents, setResetComponents] = useState(true);
-  const [cursorPosition, setCursorPosition] = useState(0);
-  const [cursorWidth, setCursorWidth] = useState(14);
-  const [currentText, setCurrentText] = useState("");
-  const [fontsizeValue, setFontsizeValue] = useState<number>(typingFontsizeX);
-
-  const { showTypingSetting } = useLayoutStates();
-
-  const listCursorShape: {
+  const cursorShapeList: {
     styles: CursorStyles;
     cursorWidth: number;
     cursorHeight?: number;
@@ -110,36 +99,28 @@ const TextAppearance = ({ show }: any) => {
       }`,
     },
   ];
+  const fontSizeList: FontSizeTypes[] = [
+    makeFraction(0.5),
+    makeFraction(1),
+    makeFraction(2),
+    makeFraction(3),
+    makeFraction(4),
+  ];
+
+  const [value, setValue] = useState("");
+  const [typingWordIndex, setTypingWordIndex] = useState(0);
+  const [cursorPosition, setCursorPosition] = useState(0);
+  const [cursorWidth, setCursorWidth] = useState(14);
+  const [currentText, setCurrentText] = useState("");
+  const [resetComponents, setResetComponents] = useState(true);
+
+  const [fontsizeValue, setFontsizeValue] = useState<number>(typingFontsizeX);
+
+  const { showTypingSetting } = useLayoutStates();
 
   const handleOnChange = (e: any) => {
     if (e.target.value === " ") return;
     setValue(e.target.value);
-  };
-  const handleOnKeyDown = (e: any) => {
-    if (value.length > 0 && e.key === " ") {
-      setTypingWordIndex((pre) => pre + 1);
-      setValue("");
-    }
-    const { cursorPositionIncrease } = calculatePositionForCursor(
-      wordList[typingWordIndex],
-      value,
-      "24px"
-    );
-    if (
-      e.key.length === 1 &&
-      !e.ctrlKey &&
-      !e.metaKey &&
-      !e.altKey &&
-      e.key !== " "
-    ) {
-      if (e.key !== "Backspace") {
-        setCursorPosition(cursorPosition + cursorPositionIncrease);
-        setCursorWidth(cursorPositionIncrease);
-        setCurrentText(
-          wordList[typingWordIndex].word.split("")[value.length + 1]
-        );
-      }
-    }
   };
   const handleResetWordComponents = () => {
     setResetComponents(false);
@@ -181,21 +162,12 @@ const TextAppearance = ({ show }: any) => {
   useUpdateWordGap();
   useUpdateFirstTime(setFontsizeValue);
 
-  const fontSizeList: FontSizeTypes[] = [
-    makeFraction(0.5),
-    makeFraction(1),
-    makeFraction(2),
-    makeFraction(3),
-    makeFraction(4),
-  ];
-
   return (
     <TextBoxBorderOverlay className="w-full" title="Text appearance">
       <TypingKeyboardInput
         id="typingKeyboardId"
         hiddenInput
         value={value}
-        handleOnKeyDown={handleOnKeyDown}
         handleOnChange={handleOnChange}
       ></TypingKeyboardInput>
       {/* <p>{currentText}...</p> */}
@@ -242,7 +214,7 @@ const TextAppearance = ({ show }: any) => {
       <div>
         <TextAndContentOverlay>
           Cursor shape:
-          {listCursorShape.map((item, index) => (
+          {cursorShapeList.map((item, index) => (
             <RelativeOverlay key={index}>
               <TypingCursorNew
                 isTyping
