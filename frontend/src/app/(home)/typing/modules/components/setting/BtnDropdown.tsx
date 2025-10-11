512809626ec0e09e45a6c42205f68a7afec41aa0
tuntinotuan <tuntino666@gmail.com>
2025-10-11 22:45:03 +0700

    separate - change font family dropdown component

---
+++ frontend/src/app/(home)/typing/modules/components/setting/BtnDropdown.tsx
@@ -0,0 +1,60 @@
+import Dropdown from "@/components/dropdown/Dropdown";
+import GreenTickIcon from "@/components/icons/GreenTickIcon";
+import { useTyping } from "@/contexts/TypingStates";
+import React from "react";
+
+const BtnDropdown = () => {
+  const fontFamilyList = [
+    { name: "Roboto", code: "font-roboto" },
+    { name: "Roboto Mono", code: "font-robotoMono" },
+    { name: "DM Mono", code: "font-dmMono" },
+    { name: "Ubuntu", code: "font-ubuntu" },
+    { name: "Coming Soon", code: "font-comingSoon" },
+    { name: "Pacifico", code: "font-pacifico" },
+    { name: "Borel", code: "font-borel" },
+    { name: "Oxygen", code: "font-oxygen" },
+    { name: "Lalezar", code: "font-lalezar" },
+    { name: "Lato", code: "font-lato" },
+    { name: "JetBrains Mono", code: "font-jetBrainsMono" },
+    { name: "Itim", code: "font-itim" },
+    { name: "Inconsolata", code: "font-inconsolata" },
+    { name: "Rubik Bubbles", code: "font-rubikBubbles" },
+    { name: "IBM Plex Sans", code: "font-iBMPlexSans" },
+    { name: "IBM Plex Mono", code: "font-iBMPlexMono" },
+    { name: "Geist Mono", code: "font-geistMono" },
+    { name: "Geist", code: "font-geist" },
+    { name: "Fira Code", code: "font-firaCode" },
+    { name: "Courier Prime", code: "font-courierPrime" },
+  ];
+  const { fontFamily, setFontFamily } = useTyping();
+  return (
+    <Dropdown
+      name={fontFamily?.name || "Choose your fontsize"}
+      className="border border-transparent bg-typingBgControlMenu text-white"
+      activeClassName="border-b-typingColorActive"
+    >
+      <div className="max-h-48 bg-typingBgControlMenu rounded-b-md overflow-y-auto [&::-webkit-scrollbar]:w-[5px] [&::-webkit-scrollbar-track]:bg-typingBg [&::-webkit-scrollbar-thumb]:bg-typingColorActive [&::-webkit-scrollbar-track]:rounded-sm [&::-webkit-scrollbar-thumb]:rounded-sm">
+        {fontFamilyList.map((item) => (
+          <div
+            className={`${
+              item.code
+            } flex items-center justify-between p-2 hover:bg-typingColorActive transition-all last:rounded-b-md ${
+              fontFamily?.name === item.name ? "bg-typingBg font-bold" : ""
+            }`}
+            onClick={() => setFontFamily(item)}
+          >
+            <p className="brightness-75">{item.name}</p>
+            {fontFamily?.name === item.name && (
+              <GreenTickIcon
+                className=" text-green-500"
+                fontSize="inherit"
+              ></GreenTickIcon>
+            )}
+          </div>
+        ))}
+      </div>
+    </Dropdown>
+  );
+};
+
+export default BtnDropdown;
--- frontend/src/app/(home)/typing/modules/components/setting/TextAppearance.tsx
+++ frontend/src/app/(home)/typing/modules/components/setting/TextAppearance.tsx
@@ -20,8 +20,7 @@ import { useUpdateWordGap } from "../../../func/setting/useUpdateWordGap";
 import { useUpdateSettingCursorPosition } from "../../../func/setting/useUpdateSettingCursorPosition";
 import { useUpdateFirstTime } from "../../../func/setting/useUpdateFirstTime";
 import InputChangeFontsize from "./InputChangeFontsize";
-import Dropdown from "@/components/dropdown/Dropdown";
-import GreenTickIcon from "@/components/icons/GreenTickIcon";
+import BtnDropdown from "./BtnDropdown";
 
 const TextAppearance = ({ show }: any) => {
   const {
@@ -111,28 +110,6 @@ const TextAppearance = ({ show }: any) => {
     makeFraction(2),
     makeFraction(3),
     makeFraction(4),
-  ];
-  const fontFamilyList = [
-    { name: "Roboto", code: "font-roboto" },
-    { name: "Roboto Mono", code: "font-robotoMono" },
-    { name: "DM Mono", code: "font-dmMono" },
-    { name: "Ubuntu", code: "font-ubuntu" },
-    { name: "Coming Soon", code: "font-comingSoon" },
-    { name: "Pacifico", code: "font-pacifico" },
-    { name: "Borel", code: "font-borel" },
-    { name: "Oxygen", code: "font-oxygen" },
-    { name: "Lalezar", code: "font-lalezar" },
-    { name: "Lato", code: "font-lato" },
-    { name: "JetBrains Mono", code: "font-jetBrainsMono" },
-    { name: "Itim", code: "font-itim" },
-    { name: "Inconsolata", code: "font-inconsolata" },
-    { name: "Rubik Bubbles", code: "font-rubikBubbles" },
-    { name: "IBM Plex Sans", code: "font-iBMPlexSans" },
-    { name: "IBM Plex Mono", code: "font-iBMPlexMono" },
-    { name: "Geist Mono", code: "font-geistMono" },
-    { name: "Geist", code: "font-geist" },
-    { name: "Fira Code", code: "font-firaCode" },
-    { name: "Courier Prime", code: "font-courierPrime" },
   ];
 
   const [value, setValue] = useState("");
@@ -285,34 +262,7 @@ const TextAppearance = ({ show }: any) => {
         </TextAndContentOverlay>
         <TextAndContentOverlay>
           Font family:
-          <Dropdown
-            name={fontFamily?.name || "Choose your fontsize"}
-            className="border border-transparent bg-typingBgControlMenu text-white"
-            activeClassName="border-b-typingColorActive"
-          >
-            <div className="max-h-48 bg-typingBgControlMenu rounded-b-md overflow-y-auto [&::-webkit-scrollbar]:w-[5px] [&::-webkit-scrollbar-track]:bg-typingBg [&::-webkit-scrollbar-thumb]:bg-typingColorActive [&::-webkit-scrollbar-track]:rounded-sm [&::-webkit-scrollbar-thumb]:rounded-sm">
-              {fontFamilyList.map((item) => (
-                <div
-                  className={`${
-                    item.code
-                  } flex items-center justify-between p-2 hover:bg-typingColorActive transition-all last:rounded-b-md ${
-                    fontFamily?.name === item.name
-                      ? "bg-typingBg font-bold"
-                      : ""
-                  }`}
-                  onClick={() => setFontFamily(item)}
-                >
-                  <p className="brightness-75">{item.name}</p>
-                  {fontFamily?.name === item.name && (
-                    <GreenTickIcon
-                      className=" text-green-500"
-                      fontSize="inherit"
-                    ></GreenTickIcon>
-                  )}
-                </div>
-              ))}
-            </div>
-          </Dropdown>
+          <BtnDropdown />
         </TextAndContentOverlay>
       </div>
     </TextBoxBorderOverlay>
