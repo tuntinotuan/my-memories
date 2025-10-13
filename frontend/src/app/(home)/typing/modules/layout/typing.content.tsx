b407569ff0183e20ab0ae2d62554c20ef9b29cfe
tuntinotuan <tuntino666@gmail.com>
2025-10-13 22:40:08 +0700

    update - logic loading typing page skeleton

--- frontend/src/app/(home)/typing/modules/layout/typing.content.tsx
+++ frontend/src/app/(home)/typing/modules/layout/typing.content.tsx
@@ -1,14 +1,11 @@
 "use client";
 import TypingRestart from "@/app/(home)/typing/modules/components/TypingRestart";
-import { TypingOnlyAWord } from "./typing.only.a.word";
-import { TypingManyWords } from "./typing.many.words";
 import { useTyping } from "@/contexts/TypingStates";
 import { typingWordsTypes } from "@/api/typing/typing.type";
 import TypingViewAmountOrTime from "../components/TypingViewAmountOrTime";
 import { useHydrate } from "../../func/useHydrate";
 import { TypingManyWordsV2 } from "./typing.many.words.v2";
 import { TypingOnlyAWordV2 } from "./typing.only.a.word.v2";
-import TypingPageSkeleton from "@/components/skeleton/TypingPageSkeleton";
 
 export const TypingContent = ({ data }: { data: typingWordsTypes[] }) => {
   const {
@@ -21,7 +18,7 @@ export const TypingContent = ({ data }: { data: typingWordsTypes[] }) => {
     typingSettingLocal,
   } = useTyping();
   const { hydrated, setHydrated } = useHydrate();
-  if (!hydrated) return <TypingPageSkeleton />; // or a skeleton/placeholder
+  if (!hydrated) return null; // or a skeleton/placeholder
   return (
     <div
       className={`w-full h-full flex flex-col items-center gap-4 overflow-hidden ${
--- frontend/src/app/(home)/typing/page.tsx
+++ frontend/src/app/(home)/typing/page.tsx
@@ -18,7 +18,7 @@ export default function TypingPage() {
 
   return (
     <div className="flex flex-col w-full gap-2 px-4 pt-2 bg-typingBg text-white">
-      {/* <TypingPageSkeleton></TypingPageSkeleton> */}
+      {loadingTypingWordList && <TypingPageSkeleton></TypingPageSkeleton>}
       {!showResults && !loadingTypingWordList && (
         <TypingHeaderMenu></TypingHeaderMenu>
       )}
