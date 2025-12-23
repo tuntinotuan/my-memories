dad7a5dea04ef445aea989a3ddb2ff6ed5e11b70
tuntinotuan <tuntino666@gmail.com>
2025-12-23 22:20:05 +0700

    stop typing after cursor move

--- frontend/src/contexts/TypingStates.tsx
+++ frontend/src/contexts/TypingStates.tsx
@@ -345,13 +345,14 @@ export const TypingProvider = ({ children }: { children: React.ReactNode }) => {
 
   useEffect(() => {
     const handleMove = (e: MouseEvent) => {
+      if (!cursorIsTyping) return;
       console.log("X:", e.clientX, "Y:", e.clientY);
-      // setCursorIsTyping(false);
+      setCursorIsTyping(false);
     };
     // document.documentElement.classList.add("cursor-none");
     window.addEventListener("mousemove", handleMove);
     return () => window.removeEventListener("mousemove", handleMove);
-  }, []);
+  }, [cursorIsTyping]);
 
   // get api from Random-Word-Api
   // useEffect(() => {
